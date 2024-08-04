07f7ca040e7df563b2d7b6efaf12326d01496e72
Sahil Uz2 <sahilbatgeri@gmail.com>
2024-08-04 13:30:23 +0530

    base added in vite config file

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/{Sahil-Batgeri-Portfolio-Website-}/',
+  base: '/Sahil-Batgeri-Portfolio-Website-/',
   plugins: [react()],
 })
